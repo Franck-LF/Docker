871a3e385c8037e9b04cbec15de5b61b5e6571b1
Franck-LF <f7lefur@gmail.com>
2025-03-21 13:43:47 +0100

    modify api3.py

--- api3.py
+++ api3.py
@@ -3,15 +3,40 @@
 #
 #
 
+
+
 import flask
 from flask import request, jsonify, render_template
+import mysql.connector
+import json
+from typing import List, Dict
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/hello', methods=['GET'])
-def test():
-    return {'status' : 'Hello, Docker!'}
+
+
+def persons() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'persons'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM persons')
+    results = [{firstname: lastname} for (firstname, lastname) in cursor]
+    cursor.close()
+    connection.close()
+    return results
+
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    return json.dumps({'persons': persons()})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

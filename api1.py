b5c939f16987cfe652a9f482512c30b435ccaac0
Franck-LF <f7lefur@gmail.com>
2025-03-21 09:20:52 +0100

    modified api1.py for 1st exercise

--- api1.py
+++ api1.py
@@ -1,11 +1,13 @@
 
-
 import flask
 from flask import request, jsonify, render_template
 
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-
-
+@app.route('/hello', methods=['GET'])
+def test():
+    return {'status' : 'OK'}
 
 if __name__ == '__main__':
-    print('ok')
+    app.run(host='0.0.0.0', port=5000)
